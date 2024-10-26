f0a79084b07bb7f12f0d550f20c1e817ff3a3885
LelonDelonMelon <burkinggs@gmail.com>
2024-10-26 21:47:47 +0300

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Password Checker CLI
 
-A Node.js command-line application to validate passwords based on configurable criteria. This tool allows you to check if a password meets certain security requirements, such as minimum length, number of digits, and number of special characters. You can specify these criteria directly via command-line arguments or through a configuration file with a `.cpc` extension.
+A Node.js command-line application to validate passwords based on configurable criteria. This tool allows checking if a password meets certain security requirements, such as minimum length, number of digits, and number of special characters. You can specify these criteria directly via command-line arguments or through a configuration file with a `.cpc` extension.
+
+![Password Checker CLI Demo](./assets/cpcdemo.gif)
 
 ## Features
 
@@ -8,43 +10,96 @@ A Node.js command-line application to validate passwords based on configurable c
 - Support for configuration files with `.cpc` extension.
 - Colored output for better readability.
 
+## Currently Supported Validation Criterias
+
+- Minimum length (`--minlength`).
+- Minimum number of digits (`--minDigits`).
+- Minimum number of special characters (`--minSpecials`).
+- Recurring character check (by default)
+
 ## Installation
 
 1. Clone the repository:
 
    ```sh
-   git clone https://github.com/yourusername/password-checker-cli.git
-   cd password-checker-cli
+   git clone https://github.com/LelonDelonMelon/cliPassCheck.git
+   cd cliPassCheck
+   ```
+
 2. Install dependencies:
+
    ```sh
-    npm install
+   npm install
+   ```
 
 ## Usage
-    You can pass the password criterias directly as cli arguments:
-        
-   ```sh
-   node index.js check --password "yourpassword" --minlength 8 --minDigits 2 --minSpecials 1
+
+You can pass the password criteria directly as CLI arguments:
+
+```sh
+node index.js check --password "yourpassword" --minlength 8 --minDigits 2 --minSpecials 1
+```
 
 ## Configuration File
 
-    Alternatively you can use a configuration file to specify the validation criteria:
+Alternatively, you can use a configuration file to specify the validation criteria:
 
-    1. Create a .cpc configuration file with the following format:
-        minlength = 8
-        minDigits = 2
-        minSpecials = 1
-    2. Run the command with the --config argument passing the path to your .cpc file:
+1. Create a `.cpc` configuration file with the following format:
 
-        ```sh
-        node index.js check --password "yourpassword" --config "./path/to/config.cpc"
+   ```
+   minlength = 8
+   minDigits = 2
+   minSpecials = 1
+   ```
+
+2. Run the command with the `--config` argument, passing the path to your `.cpc` file:
+
+   ```sh
+   node index.js check --password "yourpassword" --config "./path/to/config.cpc"
+   ```
+
+   - The default configuration file is at `./config/example.cpc`
 
 ## Configuration File Format
-    The configuration file should have a .cpc extension and contain key-value pairs for the criteria. For example:
 
-    # example.cpc
-    minlength = 8
-    minDigits = 2
-    minSpecials = 1
+The configuration file should have a `.cpc` extension and contain key-value pairs for the criteria. For example:
 
+```
+# example.cpc
+minlength = 8
+minDigits = 2
+minSpecials = 1
+```
 
+## Example
 
+Running with Command-Line Arguments
+
+```sh
+node index.js check --password "deneme123" --minlength 8 --minDigits 2 --minSpecials 1
+```
+
+Running with a Configuration File
+
+1. Create a config file called config.cpc
+
+```
+#config.cpc
+minlength = 8
+minDigits = 2
+minSpecials = 1
+```
+
+2. Run the following command:
+
+```sh
+node index.js check --password "pass123" --config "./config.cpc"
+```
+
+## Dependencies
+
+This package requires:
+
+- nodejs: ^20.x.x
+- npm: ^10.x.x
+- yargs: ^17.7.2
--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 const yargs = require("yargs");
-const colors = require('./colors');  // Assuming colors is correctly set up
-const readCpc = require('./config/configReader');  // Adjust the path as needed
+const colors = require("./colors"); // Assuming colors is correctly set up
+const readCpc = require("./config/configReader"); // Adjust the path as needed
 
 /**
  * Validate if the input password meets the criteria
  * @param {string} input - The password to check
- * @param {number} minlength - The minimum length of the password
+ * @param {number} minLength - The minimum length of the password
  * @param {number} minDigits - The minimum number of digits in the password
  * @param {number} minSpecials - The minimum number of special characters in the password
  * @returns {boolean} - True if the password is valid, false otherwise
+ * @returns {boolean} - True if the password is valid, false otherwise
  */
-function validateInput(input, minlength, minDigits, minSpecials) {
-    if (input.length < minlength) {
-        console.log(
-            `${colors.Fg.Red}Password is too short. Minimum length required is ${minlength}.${colors.Reset}`
-        );
-        return false;
-    }
+function validateInput(input, minLength, minDigits, minSpecials) {
+  if (input.length < minLength) {
+    console.log(
+      `${colors.Fg.Red}Password is too short. Minimum length required is ${minLength}.${colors.Reset}`
+    );
+    return false;
+  }
 
-    let digitCount = 0,
-        specialCharCount = 0;
+  let digitCount = 0,
+    specialCharCount = 0;
 
-    for (let char of input) {
-        if (/\d/.test(char)) digitCount++;
-        if (/\W|_/.test(char)) specialCharCount++;
-    }
+  for (let char of input) {
+    if (/\d/.test(char)) digitCount++;
+    if (/\W|_/.test(char)) specialCharCount++;
+  }
 
-    if (digitCount < minDigits) {
-        console.log(
-            `${colors.Fg.Red}Password should contain at least ${minDigits} digits.${colors.Reset}`
-        );
-        return false;
-    }
+  if (digitCount < minDigits) {
+    console.log(
+      `${colors.Fg.Red}Password should contain at least ${minDigits} digits.${colors.Reset}`
+    );
+    return false;
+  }
 
-    if (specialCharCount < minSpecials) {
-        console.log(
-            `${colors.Fg.Red}Password should contain at least ${minSpecials} special characters.${colors.Reset}`
-        );
-        return false;
-    }
+  if (specialCharCount < minSpecials) {
+    console.log(
+      `${colors.Fg.Red}Password should contain at least ${minSpecials} special characters.${colors.Reset}`
+    );
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 /**
@@ -48,62 +49,60 @@ function validateInput(input, minlength, minDigits, minSpecials) {
  * @param {object} argv - The parsed arguments
  */
 function handleCheckCommand(argv) {
-    let minlength, minDigits, minSpecials, password;
+  let minLength, minDigits, minSpecials, password;
 
-    if (argv.config) {
-        console.log(`Reading config from: ${argv.config}`);
-        const config = readCpc(argv.config);
-        minlength = parseInt(config.minlength, 10);
-        minDigits = parseInt(config.minDigits, 10);
-        minSpecials = parseInt(config.minSpecials, 10);
-        password = argv.password;
-    } else {
-        minlength = argv.minlength;
-        minDigits = argv.minDigits;
-        minSpecials = argv.minSpecials;
-        password = argv.password;
-    }
+  if (argv.config) {
+    console.log(`Reading config from: ${argv.config}`);
+    const config = readCpc(argv.config);
+    minLength = parseInt(config.minLength, 10);
+    minDigits = parseInt(config.minDigits, 10);
+    minSpecials = parseInt(config.minSpecials, 10);
+    password = argv.password;
+  } else {
+    minLength = argv.minLength;
+    minDigits = argv.minDigits;
+    minSpecials = argv.minSpecials;
+    password = argv.password;
+  }
 
-    if (!password) {
-        console.error(
-            `${colors.Fg.Red}The --password argument is required.${colors.Reset}`
-        );
-        process.exit(1);
-    }
+  if (!password) {
+    console.error(
+      `${colors.Fg.Red}The --password argument is required.${colors.Reset}`
+    );
+    process.exit(1);
+  }
 
-    const res = validateInput(password, minlength, minDigits, minSpecials);
-    if (res) console.log(`${colors.Fg.Green}Password is valid${colors.Reset}`);
+  const res = validateInput(password, minLength, minDigits, minSpecials);
+  if (res) console.log(`${colors.Fg.Green}Password is valid${colors.Reset}`);
 }
 
 const argv = yargs
-    .command({
-        command: "check",
-        describe: "Check if the input password meets the criteria",
-        builder: {
-            config: {
-                describe: "Path to the config file",
-                type: "string",
-            },
-            minlength: {
-                describe: "Minimum length of the password",
-                type: "number",
-            },
-            minDigits: {
-                describe: "Minimum number of digits in the password",
-                type: "number",
-            },
-            minSpecials: {
-                describe: "Minimum number of special characters in the password",
-                type: "number",
-            },
-            password: {
-                describe: "Password to check",
-                demandOption: true,
-                type: "string",
-            },
-        },
-        handler: handleCheckCommand,
-    })
-    .help()
-    .argv;
-
+  .command({
+    command: "check",
+    describe: "Check if the input password meets the criteria",
+    builder: {
+      config: {
+        describe: "Path to the config file",
+        type: "string",
+      },
+      minLength: {
+        describe: "Minimum length of the password",
+        type: "number",
+      },
+      minDigits: {
+        describe: "Minimum number of digits in the password",
+        type: "number",
+      },
+      minSpecials: {
+        describe: "Minimum number of special characters in the password",
+        type: "number",
+      },
+      password: {
+        describe: "Password to check",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler: handleCheckCommand,
+  })
+  .help().argv;
